87b62d26c79c3958eed66f50d6d4283570d1666f
SiqueiraX <vieiradossatoserikkauavieirado@gmail.com>
2026-01-20 12:22:51 -0300

    Delete netlify/functions directory

--- netlify/functions/envcheck.js
+++
@@ -1,9 +0,0 @@
-export async function handler() {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      hasUrl: !!process.env.SUPABASE_URL,
-      hasKey: !!process.env.SUPABASE_SERVICE_ROLE_KEY
-    })
-  }
-}

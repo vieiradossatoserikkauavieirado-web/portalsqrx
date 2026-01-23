f535a103611c296128a7d4874b00aa4f23092946
SiqueiraX <vieiradossatoserikkauavieirado@gmail.com>
2026-01-23 18:37:22 -0300

    Add files via upload

--- netlify/functions/discord-login-sub.js
+++ netlify/functions/discord-login-sub.js
@@ -1,27 +1,24 @@
-function b64url(obj) {
-  return Buffer.from(JSON.stringify(obj)).toString("base64url");
-}
-
 exports.handler = async (event) => {
   const clientId = process.env.DISCORD_CLIENT_ID;
-  const redirectUri = process.env.DISCORD_SUB_REDIRECT_URI;
+  const redirectUri = (process.env.DISCORD_SUB_REDIRECT_URI || "").trim().replace(/\/$/, "");
 
-  const returnTo =
-    (event.queryStringParameters && event.queryStringParameters.return) || "/gamemodes.html";
+  if (!clientId || !redirectUri) {
+    return { statusCode: 500, body: "missing_env: DISCORD_CLIENT_ID or DISCORD_SUB_REDIRECT_URI" };
+  }
 
-  const state = b64url({ returnTo });
+  const returnTo = event.queryStringParameters?.return || "/gamemodes.html";
 
   const params = new URLSearchParams({
     client_id: clientId,
     redirect_uri: redirectUri,
     response_type: "code",
     scope: "identify",
-    state
+    state: Buffer.from(JSON.stringify({ returnTo })).toString("base64url"),
   });
 
   return {
     statusCode: 302,
     headers: { Location: `https://discord.com/oauth2/authorize?${params.toString()}` },
-    body: ""
+    body: "",
   };
 };
